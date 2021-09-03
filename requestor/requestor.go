fcdeaba82d6b95ac70732da0b442e939cba55f0f
Waldemar Quevedo <wally@synadia.com>
2021-09-03 11:04:13 -0700

    Add app that makes request concurrently

---
+++ requestor/requestor.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	nc, err := nats.Connect("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		m.Respond([]byte("OK!"))
+	})
+
+	for i := 0; i < 5; i++ {
+		hc := &http.Client{}
+		go func() {
+			for range time.NewTicker(200 * time.Millisecond).C {
+				body := bytes.NewBuffer([]byte("{}"))
+				resp, err := hc.Post("http://localhost:3000/messages", "application/json", body)
+				if err != nil {
+					log.Println("Error: ", err)
+					continue
+				}
+				resp.Body.Close()
+				log.Println(resp.Status)
+			}
+		}()
+	}
+	select {}
+}

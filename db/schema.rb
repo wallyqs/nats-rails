6cfa94f6f2926690cf113c5c251963bf91e1013f
Waldemar Quevedo <wally@synadia.com>
2021-09-03 10:08:34 -0700

    Change to postgres
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_02_165309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "data"
     t.datetime "created_at", precision: 6, null: false
